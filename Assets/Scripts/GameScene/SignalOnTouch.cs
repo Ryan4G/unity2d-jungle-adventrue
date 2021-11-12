834b1bc78b44c6a075e228373a7b5a43060af3bd
ryan4gitee <rliang.lai@outlook.com>
2021-11-12 23:45:28 +0800

    add slice spikes and treasure images, add logic scripts.

---
+++ Assets/Scripts/GameScene/SignalOnTouch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class SignalOnTouch : MonoBehaviour
+{
+    public UnityEvent onTouch;
+
+    public bool playAudioOnTouch = true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SendSignal(collision.gameObject);
+    }
+
+    private void SendSignal(GameObject gameObject)
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+            if (playAudioOnTouch)
+            {
+                var audio = GetComponent<AudioSource>();
+
+                if (audio && audio.gameObject.activeInHierarchy)
+                {
+                    audio.Play();
+                }
+            }
+
+            onTouch.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SendSignal(collision.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
